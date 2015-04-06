bfdfc7c1d69b7cf09d2839066849bde572ba5c30
Dewitt Buckingham <dj729@comcast.net>
2015-04-05 22:45:12 -0700

    Multiplier module
    `start()`, `stop()` and `get()` methods. Pretty basic.
    
    `start()` accepts three optional arguments. `interval` is the amount of
    timer between decrementing. `max` in the maximum value that the
    multiplier starts at. `min` in the minimum value it reaches before
    stopping.

--- src/js/multiplier.js
+++ src/js/multiplier.js
@@ -1,69 +1,33 @@
-/**
- * Multiplier Module
- *
- * @namespace Multiplier
- * @memberOf Scramble
- */
-Scramble = (function ( Game ) {
+var scramble = (function ( game ) {
 
-    var Multiplier = document.querySelector('#Multiplier');
-    var parent = Multiplier.parentNode;
-    var interval;
+    var multiplier = game.multiplier = {};
+    var current, id;
 
-    Game.Multiplier = {};
+    multiplier.start = function ( interval, max, min ) {
+        var INTERVAL = interval || 1000;
+        var MAX = max || 2.2;
+        var MIN = min || 0.1;
 
+        // Set initial multiplier
+        current = MAX;
 
-    /**
-     * Display current multiplier
-     * @private
-     * @memberOf Scramble.Multiplier
-     *
-     * @function
-     */
-    var _display = function() {
-        Multiplier.textContent = 'x' + Game.Multiplier.current.toFixed(1);
-    };
-
-    /**
-     * Stop Multiplier and reset background
-     * @public
-     * @memberOf Scramble.Multiplier
-     *
-     * @function stop
-     */
-    Game.Multiplier.stop = function() {
-        clearInterval(interval);
-        parent.className = 'label label-info';
-    };
-
-    /**
-     * Start Multiplier
-     * @public
-     * @memberOf Scramble.Multiplier
-     *
-     * @function start
-     */
-    Game.Multiplier.start = function() {
-        var current = Game.Multiplier.current = 2.2;
-        _display();
-        interval = setInterval(function() {
-            if(Game.Multiplier.current > 0.1) {
-                current = Game.Multiplier.current -= 0.1;
-                _display();
-
-                if(current < 1.6 && current > 1.2) {
-                    parent.classList.remove('label-info');
-                    parent.classList.add('label-primary');
-                } else if (current < 1.2 && current > 0.8) {
-                    parent.classList.remove('label-primary');
-                    parent.classList.add('label-warning');
-                } else if (current < 0.8) {
-                    parent.classList.remove('label-warning');
-                    parent.classList.add('label-danger');
-                }
+        // start countdown
+        id = setInterval(function() {
+            if(current > MIN) {
+                current -= 0.1;
+            } else {
+                multiplier.stop();
             }
-        }, 1000);            
+        }, INTERVAL);
     };
 
-    return Game;
-}( Scramble || {} ));
+    multiplier.stop = function() {
+        clearInterval(id);
+    };
+
+    multiplier.get = function() {
+        return current.toFixed(1);
+    };
+
+    return game;
+} ( scramble || {}));
