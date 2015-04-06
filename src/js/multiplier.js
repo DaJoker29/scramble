d0886c1f090b811ed8eddd3a75ef1f9e352fc493
Dewitt Buckingham <dj729@comcast.net>
2015-04-06 01:46:57 -0700

    Helpers
    Added secondary mechanics (helpers), as well as a high score
    functionality.

--- src/js/multiplier.js
+++ src/js/multiplier.js
@@ -1,32 +1,22 @@
 var scramble = (function ( game ) {
 
     var multiplier = game.multiplier = {};
-    var current, id;
+    var current;
 
-    multiplier.start = function ( interval, max, min ) {
-        var INTERVAL = interval || 2500;
-        var MAX = max || 2.2;
-        var MIN = min || 0.1;
-
-        // Set initial multiplier
-        current = MAX;
-
-        // start countdown
-        id = setInterval(function() {
-            if(current > MIN) {
-                current -= 0.1;
-            } else {
-                multiplier.stop();
-            }
-        }, INTERVAL);
+    multiplier.add = function ( value ) {
+        current += value || 1;
     };
 
-    multiplier.stop = function() {
-        clearInterval(id);
+    multiplier.subtract = function ( value ) {
+        current -= value || 1;
+    };
+
+    multiplier.reset = function () {
+        current = 1;
     };
 
     multiplier.get = function() {
-        return current.toFixed(1);
+        return current;
     };
 
     return game;
--- src/js/scramble.js
+++ src/js/scramble.js
@@ -1,7 +1,7 @@
 var scramble = (function ( game ) {
 
     var TIME_LIMIT = 120;
-    var SCORE = 50;
+    var SCORE = 5;
 
     var core = game.core = {};
     var timer, multiplier, word, scrambler, score, scrambled, current;
@@ -11,6 +11,8 @@ var scramble = (function ( game ) {
     var scrambledEl = document.querySelector('#scrambled');
     var scoreEl = document.querySelector('#score');
     var answerEl = document.querySelector('#answer');
+    var gameEl = document.querySelector('#game');
+    var highEl = document.querySelector('#highScore');
 
     var _update = function( element, value) {
         element.textContent = value;
@@ -23,7 +25,6 @@ var scramble = (function ( game ) {
             seconds -= 1;
 
             _update( timerEl, seconds);
-            _update( multiplierEl, multiplier.get() );
         }
     };
 
@@ -37,10 +38,35 @@ var scramble = (function ( game ) {
 
     var _answerListener = function ( e ) {
         if(e.target.value.toLowerCase() === current.toLowerCase()) {
-            var points = SCORE * parseFloat( multiplier.get() );
+            var points = SCORE * multiplier.get();
             score.add(points);
-            answerEl.value = '';
+            multiplier.add();
             core.endTurn();
+        }
+    };
+
+    var _checkHelpers = function () {
+        var skip = document.querySelector('#skip');
+        var reshuffle = document.querySelector('#reshuffle');
+        var extraTime = document.querySelector('#extraTime');
+        var current = multiplier.get();
+
+        if (current > 1) {
+            skip.style.visibility = 'visible';
+        } else {
+            skip.style.visibility = 'hidden';
+        }
+
+        if ( current > 5 ) {
+            reshuffle.style.visibility = 'visible';
+        } else {
+            reshuffle.style.visibility = 'hidden';
+        }
+
+        if ( current > 10 ) {
+            extraTime.style.visibility = 'visible';
+        } else {
+            extraTime.style.visibility = 'hidden';
         }
     };
 
@@ -54,12 +80,18 @@ var scramble = (function ( game ) {
 
         // Initialize
         word.init();
+        _checkHelpers();
+        _update(highEl, localStorage.highScore || 0);
     };
 
     core.newGame = function () {
         // Initialize values
         seconds = TIME_LIMIT;
         score.reset();
+        multiplier.reset();
+
+        // Show board
+        gameEl.style.visibility = 'visible';
 
         // Start game timer
         timer.start(_timerCallback);
@@ -73,17 +105,24 @@ var scramble = (function ( game ) {
 
     core.quit = function() {
 
+        // Check high score
+        if ( localStorage.getItem('highScore') === null || parseInt( localStorage.highScore ) < score.get() ) {
+            localStorage.highScore = score.get();
+        }
+
         // Cleanup listeners
         _removeListeners();
 
         // Stop timers
         timer.stop();
-        multiplier.stop();
+
+
+        _update(highEl, localStorage.highScore || 0);
     };
 
     core.endTurn = function() {
-        // reset multiplier
-        multiplier.stop();
+        // Clear input
+        answerEl.value = '';
 
         core.startTurn();
     };
@@ -92,16 +131,38 @@ var scramble = (function ( game ) {
         // Initialize values
         word.pick();
         current = word.get();
-        console.log(current);
-
-        // start multiplier
-        multiplier.start();
+        console.log(current); // Cheater
         
         // Initialize board
         _update( scoreEl, score.get());
         _update( scrambledEl, scrambler( word.get() ) );
         _update( multiplierEl, multiplier.get());
         _update( timerEl, seconds);
+        _checkHelpers();
+    };
+
+    core.skip = function() {
+        multiplier.subtract();
+        _update( multiplierEl, multiplier.get());
+        _checkHelpers();
+
+        core.endTurn();
+    };
+
+    core.reshuffle = function () {
+        multiplier.subtract( 5 );
+        _update( multiplierEl, multiplier.get());
+        _checkHelpers();
+
+        _update(scrambledEl, scrambler ( word.get() ) );
+    };
+
+    core.extraTime = function () {
+        multiplier.subtract( 10 );
+        _update( multiplierEl, multiplier.get());
+        _checkHelpers();
+
+        seconds += 60;
     };
 
     return game;
